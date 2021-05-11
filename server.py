ddff839ae5fa73ebdfe08569671f3b52b9977c2e
Emmanuel Sobamowo <sobamowo.laolu@hotmail.com>
2021-05-11 15:06:57 +0100

    Add hashids

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+from hashids import Hashids
 
 server = Flask(__name__)
 CORS(server)
@@ -10,3 +10,10 @@ server.run(debug=True)
 @server.route('/') 
 def home(): 
     return jsonify({})
+
+hashids = Hashids(salt= 'this is my salt')
+id = hashids.encode(1, 2, 3)
+numbers = hashids.decode(id)
+
+print(id)
+print(numbers)
