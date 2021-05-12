72e84a660304ec044c8c2f9d40e40ef35959a91c
Emmanuel Sobamowo <sobamowo.laolu@hotmail.com>
2021-05-12 10:50:31 +0100

    Make changes to the js file

--- app.py
+++ app.py
@@ -21,25 +21,28 @@ def get_db_connection():
     return connection
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=('GET', 'POST'))
 def home():
     connection = get_db_connection()
 
     if request.method == 'POST':
         url = request.form['url']
+
         if not url:
-            flash('The URL is required!')
+            flash('The URL is needed!')
             return redirect(url_for('home'))
+
         url_data = connection.execute('INSERT INTO url_db (original_url) VALUES (?)',
                                 (url,))
         connection.commit()
         connection.close()
+
         url_id = url_data.lastrowid
         hashid = hashids.encode(url_id)
         short_url = request.host_url + hashid
-        return render_template('base.html', short_url=short_url)
+        return render_template('index.html', short_url=short_url)
 
-    return render_template('base.html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
--- static/index.js
+++ static/index.js
@@ -2,13 +2,7 @@ function handleSubmit(e) {
   e.preventDefault();
   let inputValue = document.querySelector("#input-value").value;
   console.log(inputValue);
-  handleInput(inputValue);
 }
 let form = document.querySelector("#url-form");
 form.addEventListener("submit", handleSubmit);
 
-
-
-function handleInput(e) {
-  // console.log(e.target.value);
-}
